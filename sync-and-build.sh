0cdb1931de5ef7aaccce1df9b1107b51be07e881
ognjenkojic13 <ognjenkojic13@gmail.com>
2026-04-02 17:46:56 +0200

    fix index.html

--- sync-and-build.sh
+++ sync-and-build.sh
@@ -7,8 +7,4 @@ cp -r "$SOURCE/public/." "$TARGET/public/"
 cp "$SOURCE/index.html" "$TARGET/index.html"
 cp "$SOURCE/vite.config.js" "$TARGET/vite.config.js"
 
-cd "$TARGET"
-npm run build
-cp "$TARGET/dist/index.html" "$TARGET/index.html"
-
 echo "Done! Now run: git add . && git commit -m 'update' && git push origin main"
